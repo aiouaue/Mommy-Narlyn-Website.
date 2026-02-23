6153fa4dd72409d8bf3f36f7e52618c70789535b
aiouaue <aiouauewxyz@gmail.com>
2026-02-23 09:26:28 +0800

    Add files via upload

--- website/Local Storage.js
+++ website/Local Storage.js
@@ -93,3 +93,13 @@ function purchase() {
 }
 
 window.addEventListener("load", updateCart); 
+
+const signInBtnLink = document.querySelector('.signInBtn-link');
+const signUpBtnLink = document.querySelector('.signUpBtn-link');
+const wrapper = document.querySelector('.wrapper');
+signUpBtnLink.addEventListener('click', () => {
+    wrapper.classList.toggle('active');
+});
+signInBtnLink.addEventListener('click', () => {
+    wrapper.classList.toggle('active');
+});
